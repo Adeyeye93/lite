52048fea326c37e1e9a157286f34f10fc9e1f463
Adeyeye Seyi <97676173+Adeyeye93@users.noreply.github.com>
2024-08-21 10:51:16 +0100

    adding project api package

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Lite.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:ace, "~> 0.19.0"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
